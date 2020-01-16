33e970aae8dbc5a6f05e1eb29479969e6b6c6313
Jonasglien <Jonasglien2@gmail.com>
2020-01-16 19:34:49 +0100

    Updates

---
+++ precache-manifest.9a93e885c5ae77d185917cd581754d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12dc3d6d43a96a127d9053503de8cd8",
+    "url": "/nettside/index.html"
+  },
+  {
+    "revision": "f9c8c7966929a1752096",
+    "url": "/nettside/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0facdacdc3897c7d4fbd",
+    "url": "/nettside/static/js/2.ccb05c4a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nettside/static/js/2.ccb05c4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9c8c7966929a1752096",
+    "url": "/nettside/static/js/main.7a6c9b6a.chunk.js"
+  },
+  {
+    "revision": "b753b842bcf5dac471b2",
+    "url": "/nettside/static/js/runtime-main.53d31f1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nettside/static/media/logo.5d5d9eef.svg"
+  }
+]);
